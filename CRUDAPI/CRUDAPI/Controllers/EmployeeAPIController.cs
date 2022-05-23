5c09262c1a483ed0445dac15af602fa2ee9bbed5
Subhash Mishra <mishra.subhash@gmail.com>
2022-05-23 12:35:41 +0530

    Add all my files

---
+++ CRUDAPI/CRUDAPI/Controllers/EmployeeAPIController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using CRUDAPI.Models;
+using Newtonsoft.Json;
+
+namespace CRUDAPI.Controllers
+{
+    [RoutePrefix("Api/Employee")]
+    public class EmployeeAPIController : ApiController
+    {
+        
+        [HttpPost]
+        [Route("InsertEmployeeDetails")]
+        public IHttpActionResult PostEmaployee(EmployeeDetail data)
+        {
+            try
+            {
+
+                var jsonData = JsonConvert.SerializeObject(data);
+
+
+                    string fileName = @"C:\Temp\Temp.txt";
+               
+                    using (var writer = File.CreateText(fileName))
+                    {
+                        writer.WriteLine(jsonData);
+                    }
+
+                return Ok();
+
+            }
+            catch(Exception)
+            {
+                return InternalServerError();
+            }
+        }
+    }
+}
---
+++ CRUDAPI/CRUDAPI/Models/EmployeeDetail.cs
@@ -0,0 +1,10 @@
+﻿
+namespace CRUDAPI.Models
+{
+
+    public partial class EmployeeDetail
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
